80416c38803535d35612b6176913b4dbcbec5bfd
ngdangdat <ngdangdat09@gmail.com>
2025-08-31 14:27:09 +0900

    chore: add low level programming

---
+++ lla/src/fd/files.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: %s <filename> \n", argv[0]);
+    return 0;
+  }
+
+  int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+  if (fd == -1) {
+    perror("open");
+    return -1;
+  }
+
+  char *mydata = "Hello there\n";
+  write(fd, mydata, strlen(mydata));
+  close(fd);
+
+  return 0;
+}
