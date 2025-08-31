35fa0cfcc0f03466d29d61fe0f614db137109302
ngdangdat <ngdangdat09@gmail.com>
2025-08-31 14:45:40 +0900

    chore: add low level programming

--- lla/src/fd/files.c
+++ lla/src/fd/files.c
@@ -3,9 +3,15 @@
 #include <unistd.h>
 #include <string.h>
 
+struct database_header_t {
+  unsigned short version;
+  unsigned short employees;
+  unsigned int filesize;
+};
+
 
 int main(int argc, char *argv[]) {
-
+  struct database_header_t head = {0};
   if (argc != 2) {
     printf("Usage: %s <filename> \n", argv[0]);
     return 0;
@@ -16,9 +22,15 @@ int main(int argc, char *argv[]) {
     perror("open");
     return -1;
   }
+  if (read(fd, &head, sizeof(head)) != sizeof (head)) {
+    perror("read");
+    close(fd);
+    return -1;
+  }
+  printf("Database version: %d\n", head.version);
+  printf("Database employees: %d\n", head.employees);
+  printf("Database filesize: %d\n", head.filesize);
 
-  char *mydata = "Hello there\n";
-  write(fd, mydata, strlen(mydata));
   close(fd);
 
   return 0;
