0f257a1ec68d1c115d611681dd564531fb78a20b
ngdangdat <ngdangdat09@gmail.com>
2025-12-10 00:45:50 +0900

    chore: update entrypoint

--- lla-zero2hero-c/prelearning/src/main.c
+++ lla-zero2hero-c/prelearning/src/main.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-const char* HELP = "./<program> dbfile";
+const char* HELP = "./<program> dbfile (read|write) [write-info]";
 
 int main(int argc, char** argv)
 {
   // read from file and write to file
-  if (argc != 2) {
+  if (argc < 3) {
     printf("%s\n", HELP);
     return 1;
   }
+  char *dataFile = argv[1];
+  char *command= argv[2];
+
+  if (strcmp(command, "read")) {
+
+  } else if (strcmp(command, "write")) {
+    if (argc == 3) {
+      printf("[write-info] is in need for write\n");
+      return 1;
+    }
+  }
+
   return 0;
 }
